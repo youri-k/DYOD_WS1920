5f62e7870e1b11dc2dfe147f4176694d13b8feee
Youri K <youri.kaminsky@student.hpi.de>
2019-10-25 10:56:22 +0200

    implement emplace_chunk

--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -74,8 +74,17 @@ const std::string& Table::column_name(ColumnID column_id) const { return _column
 
 const std::string& Table::column_type(ColumnID column_id) const { return _column_types.at(column_id); }
 
-Chunk& Table::get_chunk(ChunkID chunk_id) { return *(_chunks[chunk_id]); }
+Chunk& Table::get_chunk(ChunkID chunk_id) { return *(_chunks.at(chunk_id)); }
 
-const Chunk& Table::get_chunk(ChunkID chunk_id) const { return *(_chunks[chunk_id]); }
+const Chunk& Table::get_chunk(ChunkID chunk_id) const { return *(_chunks.at(chunk_id)); }
+
+    void Table::emplace_chunk(Chunk chunk) {
+        if(row_count() == 0) {
+            _chunks.at(0) = std::shared_ptr<Chunk>(&chunk);
+        } else {
+            _chunks.push_back(std::shared_ptr<Chunk>(&chunk));
+        }
+
+    }
 
 }  // namespace opossum
